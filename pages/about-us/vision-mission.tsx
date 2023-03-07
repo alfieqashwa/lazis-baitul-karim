e05cec64b4e9a0402d491b8ac2af3f03e4671283
alfieqashwa <alfieqashwa@gmail.com>
2023-03-07 12:43:10 +0700

    update visi dan misi

--- pages/about-us/vision-mission.tsx
+++ pages/about-us/vision-mission.tsx
@@ -22,10 +22,31 @@ export default function VissionPage() {
           <h2 className="text-2xl font-extrabold leading-tight tracking-tighter sm:text-2xl md:text-4xl lg:text-5xl">
             Visi dan Misi
           </h2>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-          vitae, pariatur odit eligendi itaque voluptas tenetur adipisci
-          corrupti amet. Voluptatibus libero dolore quia id ipsa perspiciatis
-          nulla fuga quaerat recusandae.
+          <section className="mt-8 space-y-8 px-4 text-lg tracking-wide">
+            <article className="space-y-2">
+              <h3 className="text-xl font-bold">Visi</h3>
+              <p>
+                Menjadi Lembaga Amil Zakat, Infaq dan Shodaqoh yang amanah dan
+                profesional dalam membangun kemandirian umat, khususnya yatim
+                dan dhu’afa
+              </p>
+            </article>
+            <article className="space-y-2">
+              <h3 className="text-xl font-bold">Misi</h3>
+              <ol className="list-outside list-decimal pl-5">
+                <li>
+                  Membangun kemandirian umat, khususnya yatim dan dhu’afa.
+                </li>
+                <li>
+                  Meningkatkan sinergi dengan masyarakat dan sumber daya lainnya
+                  untuk kemandirian umat, anak-anak yatim dan dhu’afa.
+                </li>
+                <li>
+                  Meningkatkan sinergi, jaringan dan profesionalitas organisasi.
+                </li>
+              </ol>
+            </article>
+          </section>
         </div>
       </section>
     </Layout>
