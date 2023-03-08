dd41316cd53a5c5302e0a9489ca1d6e6d440a437
alfieqashwa <alfieqashwa@gmail.com>
2023-03-08 18:17:27 +0700

    fix: styling mobile view

--- pages/about-us/organization.tsx
+++ pages/about-us/organization.tsx
@@ -28,35 +28,35 @@ export default function OrganizationPage() {
                   loading="lazy"
                   src="/img/leader/mul.jpg"
                   alt="Sri Mulyani"
-                  className="z-10 h-24 w-24 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4"
+                  className="z-10 h-20 w-20 shrink-0 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4 sm:h-24 sm:w-24"
                 />
-                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow">
-                  <div className="flex flex-col items-center justify-between pt-1">
-                    <h3 className="text-lg font-semibold text-slate-900">
+                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow sm:w-52">
+                  <div className="flex flex-col items-center justify-between pt-2">
+                    <h3 className="text-md font-semibold text-slate-900 sm:text-lg">
                       Sri Mulyani
                     </h3>
-                    <h4 className="text-md font-bold uppercase text-sky-700">
+                    <h4 className="sm:text-md text-sm font-bold uppercase text-sky-700">
                       direktur
                     </h4>
                   </div>
                 </div>
               </section>
             </article>
-            <article className="thom flex flex-col justify-center py-4 sm:flex-row  sm:justify-between">
+            <article className="thom justift-start flex flex-col items-center py-4 sm:flex-row sm:justify-between sm:px-4 md:px-12">
               <section className="flex items-center justify-start p-2">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   loading="lazy"
                   src="/img/leader/nandi.jpg"
                   alt="Sunardi Sofjan"
-                  className="z-10 h-24 w-24 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4"
+                  className="z-10 h-20 w-20 shrink-0 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4 sm:h-24 sm:w-24"
                 />
-                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow">
-                  <div className="flex flex-col items-center justify-between pt-1">
-                    <h3 className="text-lg font-semibold text-slate-900">
-                      Sunardi Sofjan
+                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow sm:w-52">
+                  <div className="flex flex-col items-center justify-between pt-2">
+                    <h3 className="text-md font-semibold text-slate-900 sm:text-lg">
+                      Sunardi Sofjan, SE
                     </h3>
-                    <h4 className="text-md font-bold uppercase text-sky-700">
+                    <h4 className="sm:text-md text-sm font-bold uppercase text-sky-700">
                       wakil direktur
                     </h4>
                   </div>
@@ -68,14 +68,14 @@ export default function OrganizationPage() {
                   loading="lazy"
                   src="/img/leader/juita.jpg"
                   alt="Juita Suar"
-                  className="z-10 h-24 w-24 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4"
+                  className="z-10 h-20 w-20 shrink-0 rounded-full object-cover shadow ring-2 ring-teal-600 ring-offset-4 sm:h-24 sm:w-24"
                 />
-                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow">
-                  <div className="flex flex-col items-center justify-between pt-1">
-                    <h3 className="text-lg font-semibold text-slate-900">
-                      Juita Suar
+                <div className="relative -ml-1 h-16 w-48 rounded-r-full bg-slate-50 shadow sm:w-52">
+                  <div className="flex flex-col items-center justify-between pt-2">
+                    <h3 className="text-md font-semibold text-slate-900 sm:text-lg">
+                      Juita Suar, S.PD
                     </h3>
-                    <h4 className="text-md font-bold uppercase text-sky-700">
+                    <h4 className="sm:text-md text-sm font-bold uppercase text-sky-700">
                       sekretaris
                     </h4>
                   </div>
