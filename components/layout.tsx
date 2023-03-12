3ea48465342b8de23068686903663ebb2b8e9090
alfieqashwa <alfieqashwa@gmail.com>
2023-03-12 19:18:37 +0700

    fix: whatsapp icon

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,12 +9,12 @@ export function Layout({ children }: LayoutProps) {
   return (
     <>
       <SiteHeader />
-      <main className="mb-16 min-w-[320px]">{children}</main>
-      <footer className="fixed bottom-0 z-20 w-full bg-slate-900 py-3">
-        <div className="text-center text-xs font-medium text-slate-400 dark:text-slate-400">
+      <main className="mb-14 min-w-[320px]">{children}</main>
+      <footer className="fixed bottom-0 z-20 w-full bg-slate-100 py-1 dark:bg-slate-800">
+        <div className="text-center text-xs font-medium text-slate-600 dark:text-slate-400">
           Copyright © {new Date().getFullYear()} by{" "}
           <a
-            className="tracking-wider text-slate-200 transition hover:text-slate-50 dark:text-slate-200 dark:hover:text-slate-100"
+            className="tracking-wider text-slate-700 transition hover:text-slate-800 dark:text-slate-200 dark:hover:text-slate-100"
             href="https://github.com/alfieqashwa"
             target="_blank"
             rel="noopener noreferrer"
