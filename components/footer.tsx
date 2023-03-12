01cbe4f488b6d59c8120383a84315f46facd2557
alfieqashwa <alfieqashwa@gmail.com>
2023-03-12 19:21:01 +0700

    chore: refactor footer

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+export const Copyright = () => (
+  <footer className="fixed bottom-0 z-20 w-full bg-slate-100 py-1 dark:bg-slate-800">
+    <div className="text-center text-xs font-medium text-slate-600 dark:text-slate-400">
+      Copyright © {new Date().getFullYear()} by{" "}
+      <a
+        className="tracking-wider text-slate-700 transition hover:text-slate-800 dark:text-slate-200 dark:hover:text-slate-100"
+        href="https://github.com/alfieqashwa"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Alfie Qashwa
+      </a>
+    </div>
+  </footer>
+)
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,7 @@
+import { Footer } from "@mantine/core"
+
 import { SiteHeader } from "@/components/site-header"
+import { Copyright } from "./footer"
 import { Whatsapp } from "./whatsapp"
 
 interface LayoutProps {
@@ -10,20 +13,8 @@ export function Layout({ children }: LayoutProps) {
     <>
       <SiteHeader />
       <main className="mb-14 min-w-[320px]">{children}</main>
-      <footer className="fixed bottom-0 z-20 w-full bg-slate-100 py-1 dark:bg-slate-800">
-        <div className="text-center text-xs font-medium text-slate-600 dark:text-slate-400">
-          Copyright © {new Date().getFullYear()} by{" "}
-          <a
-            className="tracking-wider text-slate-700 transition hover:text-slate-800 dark:text-slate-200 dark:hover:text-slate-100"
-            href="https://github.com/alfieqashwa"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Alfie Qashwa
-          </a>
-        </div>
-      </footer>
       <Whatsapp />
+      <Copyright />
     </>
   )
 }
