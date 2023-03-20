8496b123192beb79e9e3320ce72b53ecae072e02
alfieqashwa <alfieqashwa@gmail.com>
2023-03-20 16:57:09 +0700

    chore: modify header title

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,18 @@
 import Head from "next/head"
 
-export const Header = ({ title }: { title: string }) => (
-  <Head>
-    <title>{title} | LAZIS Baitul Karim</title>
-    <meta name="description" content="Lembaga Amil Zakat Infaq dan Shodaqoh" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-)
+type HeaderProps = { title?: string }
+
+export const Header = ({ title }: HeaderProps) => {
+  const headTitle = `${title} | LAZIS Baitul Karim`
+  return (
+    <Head>
+      <title>{headTitle}</title>
+      <meta
+        name="description"
+        content="Lembaga Amil Zakat Infaq dan Shodaqoh"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
