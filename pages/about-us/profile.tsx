654837f49fe4f7d2fd1627f015ca5473d24c9321
alfieqashwa <alfieqashwa@gmail.com>
2023-03-20 16:56:35 +0700

    feat: add yatim-mandiri image

--- pages/about-us/profile.tsx
+++ pages/about-us/profile.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 import { Header } from "@/components/header"
 import { Layout } from "@/components/layout"
 
@@ -12,7 +14,7 @@ export default function ProfilePage() {
         <h2 className="gradient-title py-1 text-2xl font-extrabold sm:text-2xl md:text-4xl lg:text-5xl">
           Profil
         </h2>
-        <section className="space-y-2 text-lg tracking-wide">
+        <article className="space-y-2 text-lg tracking-wide">
           <p>
             Lembaga Amil Zakat, Infaq dan Shodaqoh (LAZIS) Baitul Karim berdiri
             pada hari Sabtu, 5 Februari 2022, ba&apos;da ashar di masjid Baitul
@@ -20,13 +22,23 @@ export default function ProfilePage() {
           </p>
           <p>
             Lahir dari sebuah ide remaja dan pemuda Masjid Baitul Karim untuk
-            membangun kemandirian umat, khususnya yatim dan dhu’afa.
+            membangun kemandirian umat, khususnya yatim dan dhu&apos;afa.
           </p>
           <p>
             LAZIS Baitul Karim berdiri atas kerjasama dengan Lembaga Amil Zakat,
             infaq dan Shodaqoh Nasional (LAZNAS) Yatim Mandiri yang berkantor
             pusat di Surabaya.
           </p>
+        </article>
+        <article className="space-y-2 text-lg tracking-wide">
+          <div className="flex justify-center py-6">
+            <Image
+              src="/img/logo/yatim-mandiri-logo.jpg"
+              alt="Yatim Mandiri"
+              width={300}
+              height={300}
+            />
+          </div>
           <p>
             Yatim Mandiri terdaftar di depkumham Republik Indonesia dengan
             <strong> Nomor : Ahu-2413.ah.01.02.2008</strong>. Yatim Mandiri juga
@@ -38,7 +50,7 @@ export default function ProfilePage() {
             Yatim Mandiri memiliki <em>52 kantor</em> layanan di{" "}
             <em>14 propinsi</em> di Indonesia.
           </p>
-        </section>
+        </article>
       </section>
     </Layout>
   )
